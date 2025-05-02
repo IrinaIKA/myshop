da91ed2703d1e0d8c5449932ab3bc4be81a28ef6
Irina <irina@example.com>
2025-05-02 12:11:19 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Добро пожаловать в MyShop!</h1>
+      <p className="mt-4 text-lg text-gray-700">Простой магазин на React + Tailwind</p>
+    </div>
+  )
+}
+
+export default App
